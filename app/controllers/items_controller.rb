4a141170da30b4ad47d8e82e809cd40f91ec58b9
NaotoToriizuka <toriizuka.com@gmail.co>
2020-08-24 08:26:30 +0900

    ActiveHash導入、モデル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,14 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
   end
 
   private
+
+  def item_params
+    params.require(:item).permit(:category_id, :condition_id, :prefecture_id, :postage_payer_id, :sipping_day_id,)
+  end
 
   def move_to_index
     unless user_signed_in?
