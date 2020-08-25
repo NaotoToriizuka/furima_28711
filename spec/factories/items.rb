d522dc3e3e4dfeff4f68200cba740964872c6e57
NaotoToriizuka <toriizuka.com@gmail.co>
2020-08-25 14:35:30 +0900

    Itemモデル単体テストコード、exampleを整理

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    association                :user
+    name                       {Faker::Lorem.sentence}
+    description                {Faker::Lorem.sentence}
+    category_id                {'2'}
+    condition_id               {'2'}
+    postage_payer_id           {'2'}
+    shipping_day_id            {'2'}
+    prefecture_id              {'2'}
+    price                      {'300'}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('public/test_image.jpg')
+  end
+
+  describe '商品出品機能' do
+    context '商品の情報が保存できるとき' do
+      it "全ての値が存在すれば保存できる" do
+        
+      end
+    end
+
+    context '商品の情報が保存できないとき' do
+      it "imageが空では保存できない" do
+      end
+      it "nameが空では保存できない" do
+      end
+      it "descriptionが空では保存できない" do
+      end
+      it "categoryを選択しなければ保存できない" do
+      end
+      it "conditionを選択しなければ保存できない" do
+      end
+      it "postage_payerを選択しなければ保存できない" do
+      end
+      it "shipping_dayを選択しなければ保存できない" do
+      end
+      it "prefectureを選択しなければ保存できない" do
+      end
+      it "priceが¥300~¥9,999,999の間でなければ保存できない" do
+      end
+    end
+  end
 end
