587ebd66b02475730852faac28dafd4dfe56abd6
NaotoToriizuka <toriizuka.com@gmail.co>
2020-08-27 18:10:24 +0900

    エラーハンドリングの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.valid?
+      @item.destroy
+      redirect_to root_path
+    else
+      render "show"
+    end
   end
 
   private
